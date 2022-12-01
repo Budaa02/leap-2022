95d217d97fc0e3854fc9f6a51f1c1ec5217c31ce
Morning <morning@mstars-lab2-06s-iMac.local>
2022-12-01 12:37:31 +0800

    Js daalgavar

---
+++ day-13/js/main.js
@@ -0,0 +1,161 @@
+// //                                      day 13
+
+// console.log(Math.PI);
+// console.log(Math.PI.toFixed(2));
+
+// const budget = 1_000_000_000;
+// console.log(budget);
+
+
+// const degrees = 45;
+// let rad = degrees * (Math.PI / 180);
+// console.log(rad.toFixed(4));
+
+// let a = 5, b = 9;
+// const diff = Math.abs(a - b);
+// console.log(diff);
+
+
+
+// //                                  Math.ceil
+// console.log(Math.ceil(0.95));
+// // output : 1
+// console.log(Math.ceil(7.004));
+// console.log(Math.ceil(4));
+// console.log(Math.ceil(-7.0004));
+
+
+
+// //                                  Math.floor
+// console.log(Math.floor(5.95));
+// console.log(Math.floor(5.05));
+// console.log(Math.floor(5));
+// console.log(Math.floor(-5.05));
+
+
+
+// //                                  Math.round 
+// console.log(Math.round(0.9));
+// console.log(Math.round(5.95), Math.round(5.5), Math.round(5.05));
+// console.log(Math.round(5.1), Math.round(5), Math.round(-5.05));
+
+
+// //                                  Math.max
+// console.log(Math.max(1, 3, 2));
+// console.log(Math.max(-2, -1, -3));
+
+
+
+// //                                  Math.pow(zereg)
+// console.log(Math.pow(2, 12));
+
+
+// //                                  Math.sqr()
+// console.log(Math.sqrt(81));
+
+
+// // let one = Number(prompt("15 ognuu"));
+// // let two = Number(prompt("24 ognuu"));
+
+// // console.log(Math.round(Math.sqrt(Math.pow(one, 2) + Math.pow(two, 2))) );
+
+// //                                  Math.random()
+
+// console.log(Math.floor(Math.random() * 6 + 1));
+
+// //                     random ashiglan 100 dotorh too uusgedeg  bolgo 
+
+// let dota = Math.floor(Number( prompt("duriin too ognuu")));
+// let csgo = Math.floor(Number( prompt("duriin too ognuu")));
+
+
+
+
+// console.log("loop starting ")
+// while(dota <= 50 && csgo <= 50){
+//     console.log(Math.floor(Math.random() * (dota - csgo) + csgo) ); 
+//     dota = count + 1;
+// }
+// let first = prompt("Give me your minimum number");
+// let second = prompt("Give me your second number");
+// let howMany = prompt("How many times")
+// const max = Math.max(first, second);
+// const min = Math.min(first, second);
+// let count = 1;
+// console.log("loop starts")
+// while (count <= Number(howMany)){
+//     console.log(Math.floor(Math.random() * (max - min) + min));
+//     count = count + 1;
+// }
+
+
+// let first = prompt("Give a number")
+// let first = prompt("Give a number")
+
+
+//                  while (nohtsol){ } false baiwal uildel hiihgui true uyd uildelhiine
+
+
+// let count = 1;
+// console.log("loop starts")
+// while (count <= 10){
+//     console.log(count);
+//     count = count + 1;
+// }
+
+// console.log("loop ended");// 
+/**
+ * Hervee bid nar count = count + 1 gedgiig bichihgui bol 
+ * INFINITE(forever) LOOP gedeg zuiltei tulgarna
+ */
+
+
+
+
+// let ask = prompt("Ta togloomoo urgeljluuleh uu?")
+
+
+// let count = 1;
+// console.log("loop start");
+// while(count <= 1){
+//     alert("bayarlaa")
+//     count = count + 1;
+// }
+// while(count <= 1){
+//     console.log('bayartai')
+//     count = count +1;
+// }
+
+// console.log("loop end")
+// console.log(ask)
+
+//                          togloom 
+// let answer = "yes"; 
+// while(answer == "yes") {
+//     answer = prompt ("Do you wanna continue the game?").toLowerCase();
+//     if (answer == "yes"){
+//         alert("Thanks")
+//     }
+// }
+// console.log("game over");
+
+//                      LOOP toonii niilber oloh 
+// let ask = Number(prompt("toogoo ognuu"))
+
+// let count = 1;
+// console.log("loop starts")
+// while (count <= 10){
+//     console.log(count);
+//     count = count + 1;
+// }
+let input = Number(prompt("Give a number"));
+let count = 1;
+let sum = 0;
+
+while (count <= input) {
+    count =  count + 1;
+    sum = sum + count;
+    console.log(count);
+    console.log(sum);
+    
+}
