9a185767c7030baa593fcf8d7f4debbb23ce0cb3
Budaa02 <tbudaa2@gmail.com>
2022-12-18 14:48:20 +0800

    daalgavar

---
+++ daalgavaruud/JS_numbers/general-triangle/js/main.js
@@ -0,0 +1,23 @@
+// console.log("tegsh")
+// let a = 5; 
+// let b = 10;
+
+// let c = Math.sqrt((a * a) + (b * b), 2)
+
+// console.log(c)
+
+
+
+
+// random tooo 
+// Math.floor(Number(prompt("utga ognuu")));
+// Number(Math.floor(prompt("utga ognuu 2")));
+let a = 4
+let b = 2
+
+let d = a - b - 1 
+console.log(d)
+
+
+let c = ((Math.random() * d) + 1) ;
+console.log(c)
